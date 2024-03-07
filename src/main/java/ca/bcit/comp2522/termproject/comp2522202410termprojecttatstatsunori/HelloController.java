ea9b3e99d65e2bf1cee6703f7fdfe2c0caf2a248
yoshidont-mind <tyoshida6@my.bcit.ca>
2024-03-07 15:12:21 -0800

    First commit. Hello World!

---
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/HelloController.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2522.termproject.comp2522202410termprojecttatstatsunori;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.bcit.comp2522.termproject.comp2522202410termprojecttatstatsunori {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.bcit.comp2522.termproject.comp2522202410termprojecttatstatsunori to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.comp2522202410termprojecttatstatsunori;
+}
