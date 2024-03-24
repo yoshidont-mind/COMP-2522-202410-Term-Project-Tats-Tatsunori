8da4301e04f46177fa51f547ddfc574ebe174c28
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-03-24 09:48:09 -0700

    Create enum for colors

---
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/Color.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2522.termproject.comp2522202410termprojecttatstatsunori;
+
+public enum Color {
+    WHITE,
+    RED,
+    BLUE,
+    YELLOW,
+    GREEN,
+    ORANGE,
+    PURPLE,
+    PINK,
+    GRAY,
+    BROWN
+}
