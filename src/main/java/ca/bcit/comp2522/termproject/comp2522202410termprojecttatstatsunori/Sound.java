46822d34aff00856b0ee6231deb5cd199f38a7f5
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-04-06 01:04:36 -0700

    Add sound effect

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/Sound.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/Sound.java
@@ -1,2 +1,33 @@
-package main.java.ca.bcit.comp2522.termproject.comp2522202410termprojecttatstatsunori;public class Sound {
+package main.java.ca.bcit.comp2522.termproject.comp2522202410termprojecttatstatsunori;
+
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.Media;
+
+import java.io.File;
+
+public class Sound {
+    private final MediaPlayer mediaPlayer;
+
+    Sound (String musicFilePath) {
+        Media media = new Media(new File(musicFilePath).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    public void playBgm() {
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+    }
+
+    public void play() {
+        mediaPlayer.play();
+    }
+
+    public void stop() {
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+    }
 }
