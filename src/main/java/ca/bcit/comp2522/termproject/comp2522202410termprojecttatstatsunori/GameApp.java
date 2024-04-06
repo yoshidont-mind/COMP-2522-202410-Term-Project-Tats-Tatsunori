9aa7f5ce36501bfa1c02cd932d1c9e14f2242499
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-04-05 21:53:14 -0700

    Add bgm

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/GameApp.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/GameApp.java
@@ -7,13 +7,18 @@ import javafx.scene.media.MediaPlayer;
 
 public class GameApp extends Application {
 
+    public static String bgmPath = "sound/tetris-theme-korobeiniki-rearranged-arr-for-music-box-184978.mp3";
+
     @Override
     public void start(Stage primaryStage) {
+        BackgroundMusic bgm = new BackgroundMusic(bgmPath);
+        bgm.play();
         GameView gameView = new GameView();
         Session session = new Session();
         GameController gameController = new GameController(gameView, session);
         gameController.startGameLoop();
         gameView.show(primaryStage);
+        primaryStage.setOnCloseRequest(event -> bgm.stop());
     }
 
     public static void main(String[] args) {
