eee1a02fcf0808878a153932fbe4fcc361a3f1de
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-04-11 20:12:37 -0700

    Create JavaDoc for Direction

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/Direction.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/Direction.java
@@ -1,5 +1,8 @@
 package main.java.ca.bcit.comp2522.termproject.comp2522202410termprojecttatstatsunori;
 
+/**
+ * Represents the possible directions of movement within a game board.
+ */
 public enum Direction {
     LEFT,
     RIGHT,
