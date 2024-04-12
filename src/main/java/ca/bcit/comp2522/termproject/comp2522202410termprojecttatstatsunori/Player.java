198c21dc30c5b78aaa04cb24409dc9cfdcbe426a
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-04-11 20:55:39 -0700

    Create JavaDoc

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojecttatstatsunori/Player.java
@@ -2,17 +2,38 @@ package main.java.ca.bcit.comp2522.termproject.comp2522202410termprojecttatstats
 
 import java.io.Serializable;
 
+/**
+ * Represents a player in the game, storing and managing player-specific data such as the best score.
+ *
+ * @author Tatsunori Marumo, Tatsuya Yoshida
+ * @version 2024
+ */
 public class Player implements Serializable {
     private int bestScore;
 
+    /**
+     * Constructs a new Player with the specified initial best score.
+     *
+     * @param bestScore The initial best score of the player, typically retrieved from a saved state.
+     */
     public Player(int bestScore) {
         this.bestScore = bestScore;
     }
 
+    /**
+     * Retrieves the best score achieved by the player.
+     *
+     * @return The best score as an integer.
+     */
     public int getBestScore() {
         return bestScore;
     }
 
+    /**
+     * Updates the best score of the player.
+     *
+     * @param bestScore The new best score, which should be the highest score the player has achieved.
+     */
     public void setBestScore(int bestScore) {
         this.bestScore = bestScore;
     }
